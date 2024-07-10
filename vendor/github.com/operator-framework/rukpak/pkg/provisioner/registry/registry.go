08b794863d04fe2d0ce80ef724b9c74d631eeb90
Todd Short <todd.short@me.com>
2024-07-10 09:43:08 -0400

    UPSTREAM: <drop>: go mod vendor

---
+++ vendor/github.com/operator-framework/rukpak/pkg/provisioner/registry/registry.go
@@ -0,0 +1,28 @@
+package registry
+
+import (
+	"context"
+	"fmt"
+	"io/fs"
+
+	"helm.sh/helm/v3/pkg/chart"
+	"helm.sh/helm/v3/pkg/chartutil"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	rukpakv1alpha2 "github.com/operator-framework/rukpak/api/v1alpha2"
+	"github.com/operator-framework/rukpak/pkg/convert"
+	"github.com/operator-framework/rukpak/pkg/provisioner/plain"
+)
+
+const (
+	// ProvisionerID is the unique registry provisioner ID
+	ProvisionerID = "core-rukpak-io-registry"
+)
+
+func HandleBundleDeployment(ctx context.Context, fsys fs.FS, bd *rukpakv1alpha2.BundleDeployment) (*chart.Chart, chartutil.Values, error) {
+	plainFS, err := convert.RegistryV1ToPlain(fsys, bd.Spec.InstallNamespace, []string{metav1.NamespaceAll})
+	if err != nil {
+		return nil, nil, fmt.Errorf("convert registry+v1 bundle to plain+v0 bundle: %v", err)
+	}
+	return plain.HandleBundleDeployment(ctx, plainFS, bd)
+}
