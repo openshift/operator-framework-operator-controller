8d46045901b911bef0c89e6b663846e2bdcdbcc1
Camila Macedo <7708031+camilamacedo86@users.noreply.github.com>
2025-03-13 15:19:00 +0000

    UPSTREAM: <carry>: Add stack detects inconsistencies and identifies patterns in catalogs
    Introduce tests and package implementations to enable running tests and generating reports that address both use cases.
    
    As a maintainer, I want to:
    
    - Validate catalogs to ensure that required criteria are enforced (e.g., a bundle must have either olm.bundle.object or olm.csv.metadata, but not both).
    - Analyze data for inconsistencies and common patterns (e.g., are channels using only candidate/fast/stable, or something else?).
    
    At the end of the tests, a summary report is generated to provide a comprehensive analysis.

---
+++ openshift/catalog-sync/pkg/catalog/images.go
@@ -0,0 +1,8 @@
+package catalog
+
+var CatalogImages = map[string]string{
+	"registry.redhat.io/redhat/community-operator-index:v4.18": "community",
+	"registry.redhat.io/redhat/redhat-marketplace-index:v4.18": "marketplace",
+	"registry.redhat.io/redhat/certified-operator-index:v4.18": "certified",
+	"registry.redhat.io/redhat/redhat-operator-index:v4.18":    "redhat",
+}
