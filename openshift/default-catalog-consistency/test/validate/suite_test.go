982f0a865606296e0f4acda06480aad089268ea1
Camila Macedo <7708031+camilamacedo86@users.noreply.github.com>
2025-05-29 11:28:00 +0100

    UPSTREAM: <carry>: [Default Catalog Consistency Test] (feat) add check to validate multi-arch support

--- openshift/default-catalog-consistency/test/validate/suite_test.go
+++ openshift/default-catalog-consistency/test/validate/suite_test.go
@@ -10,6 +10,7 @@ import (
 	. "github.com/onsi/gomega"
 
 	"github.com/containers/image/v5/types"
+	specsgov1 "github.com/opencontainers/image-spec/specs-go/v1"
 
 	"github/operator-framework-operator-controller/openshift/default-catalog-consistency/pkg/check"
 	"github/operator-framework-operator-controller/openshift/default-catalog-consistency/pkg/extract"
@@ -28,21 +29,7 @@ var _ = Describe("Check Catalog Consistency", func() {
 	Expect(images).ToNot(BeEmpty(), "no images found")
 	authPath := os.Getenv("REGISTRY_AUTH_FILE")
 
-	// Force image resolution to Linux to avoid OS mismatch errors on macOS,
-	// like: "no image found for architecture 'arm64', OS 'darwin'".
-	//
-	// Setting OSChoice = "linux" ensures we always get a Linux image,
-	// even when running on macOS.
-	//
-	// This skips the full multi-arch index and gives us just one manifest.
-	// To check all supported architectures (e.g., amd64, arm64, ppc64le, s390x),
-	// we’d need to avoid setting OSChoice and inspect the full index manually.
-	//
-	// TODO: Update this to support checking all architectures.
-	// See: https://issues.redhat.com/browse/OPRUN-3793
-	sysCtx := &types.SystemContext{
-		OSChoice: "linux",
-	}
+	sysCtx := &types.SystemContext{}
 	if authPath != "" {
 		fmt.Println("Using registry auth file:", authPath)
 		sysCtx.AuthFilePath = authPath
@@ -50,10 +37,28 @@ var _ = Describe("Check Catalog Consistency", func() {
 
 	for _, url := range images {
 		name := utils.ImageNameFromRef(url)
+		ctx := context.Background()
+
+		It(fmt.Sprintf("validates multiarch support for image: %s", name), func() {
+			By(fmt.Sprintf("Validating image: %s", url))
+			err := check.ImageSupportsMultiArch(
+				url,
+				check.RequiredPlatforms,
+				sysCtx,
+			).Fn(ctx, specsgov1.Descriptor{}, nil)
+			Expect(err).ToNot(HaveOccurred())
+		})
 
 		It(fmt.Sprintf("validates image: %s", name), func() {
-			ctx := context.Background()
 			By(fmt.Sprintf("Validating image: %s", url))
+			// Force image resolution to Linux to avoid OS mismatch errors on macOS,
+			// like: "no image found for architecture 'arm64', OS 'darwin'".
+			//
+			// Setting OSChoice = "linux" ensures we always get a Linux image,
+			// even when running on macOS.
+			//
+			// This skips the full multi-arch index and gives us just one manifest.
+			sysCtx.OSChoice = "linux"
 
 			extractedImage, err := extract.UnpackImage(ctx, url, name, sysCtx)
 			Expect(err).ToNot(HaveOccurred())
