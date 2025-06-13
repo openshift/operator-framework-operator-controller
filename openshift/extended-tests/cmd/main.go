717fe2506c9cc617ebf551bbdcc05d15b418a9fd
Camila Macedo <7708031+camilamacedo86@users.noreply.github.com>
2025-06-13 18:43:16 +0100

    UPSTREAM: <carry>: Add OTE for origin tests

---
+++ openshift/extended-tests/cmd/main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/openshift-eng/openshift-tests-extension/pkg/cmd"
+	e "github.com/openshift-eng/openshift-tests-extension/pkg/extension"
+	et "github.com/openshift-eng/openshift-tests-extension/pkg/extension/extensiontests"
+	g "github.com/openshift-eng/openshift-tests-extension/pkg/ginkgo"
+	// If using ginkgo, import your tests here
+	_ "github.com/openshift-eng/openshift-tests-extension/test/example"
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	registry := e.NewRegistry()
+	ext := e.NewExtension("openshift", "payload", "olmv1-origin-tests-extension")
+
+	ext.AddSuite(
+		e.Suite{
+			Name:    "example/tests",
+			Parents: []string{"openshift/conformance/parallel"},
+		})
+
+	// The tests that a suite is composed of can be filtered by CEL expressions. By
+	// default, the qualifiers only apply to tests from this extension.
+	ext.AddSuite(e.Suite{
+		Name: "example/fast",
+		Qualifiers: []string{
+			`!labels.exists(l, l=="SLOW")`,
+		},
+	})
+
+	// Global suites' qualifiers will apply to all tests available, even
+	// those outside of this extension (when invoked by origin).
+	ext.AddGlobalSuite(e.Suite{
+		Name: "example/slow",
+		Qualifiers: []string{
+			`labels.exists(l, l=="SLOW")`,
+		},
+	})
+
+	// If using Ginkgo, build test specs automatically
+	specs, err := g.BuildExtensionTestSpecsFromOpenShiftGinkgoSuite()
+	if err != nil {
+		panic(fmt.Sprintf("couldn't build extension test specs from ginkgo: %+v", err.Error()))
+	}
+
+	// Environment selector information can be added to test specs to support filtering by environment
+	specs.Select(et.NameContains("[sig-testing] openshift-tests-extension should support test-skips via environment flags")).
+		Include(et.PlatformEquals("aws"))
+
+	// You can add hooks to run before/after tests. There are BeforeEach, BeforeAll, AfterEach,
+	// and AfterAll. "Each" functions must be thread safe.
+	//
+	// specs.AddBeforeAll(func() {
+	// 	initializeTestFramework()
+	// })
+	//
+	// specs.AddBeforeEach(func(spec ExtensionTestSpec) {
+	//	if spec.Name == "my test" {
+	//		// do stuff
+	//	}
+	// })
+	//
+	// specs.AddAfterEach(func(res *ExtensionTestResult) {
+	// 	if res.Result == ResultFailed && apiTimeoutRegexp.Matches(res.Output) {
+	// 		res.AddDetails("api-timeout", collectDiagnosticInfo())
+	// 	}
+	// })
+
+	// You can also manually build a test specs list from other testing tooling
+	// TODO: example
+
+	// Modify specs, such as adding a label to all specs
+	// 	specs = specs.AddLabel("SLOW")
+
+	// Specs can be globally filtered...
+	// specs = specs.MustFilter([]string{`name.contains("filter")`})
+
+	// Or walked...
+	// specs = specs.Walk(func(spec *extensiontests.ExtensionTestSpec) {
+	//	if strings.Contains(e.Name, "scale up") {
+	//		e.Labels.Insert("SLOW")
+	//	}
+	//
+	// Specs can also be selected...
+	// specs = specs.Select(et.NameContains("slow test")).AddLabel("SLOW")
+	//
+	// Or with "any" (or) matching selections
+	// specs = specs.SelectAny(et.NameContains("slow test"), et.HasLabel("SLOW"))
+	//
+	// Or with "all" (and) matching selections
+	// specs = specs.SelectAll(et.NameContains("slow test"), et.HasTagWithValue("speed", "slow"))
+	//
+	// There are also Must* functions for any of the above flavors of selection
+	// which will return an error if nothing is found
+	// specs, err = specs.MustSelect(et.NameContains("slow test")).AddLabel("SLOW")
+	// if err != nil {
+	//    logrus.Warn("no specs found: %w", err)
+	// }
+	// Test renames
+	//	if spec.Name == "[sig-testing] openshift-tests-extension has a test with a typo" {
+	//		spec.OriginalName = `[sig-testing] openshift-tests-extension has a test with a tpyo`
+	//	}
+	//
+	// Filter by environment flags
+	// if spec.Name == "[sig-testing] openshift-tests-extension should support defining the platform for tests" {
+	//		spec.Include(et.PlatformEquals("aws"))
+	//		spec.Exclude(et.And(et.NetworkEquals("ovn"), et.TopologyEquals("ha")))
+	//	}
+	// })
+
+	ext.AddSpecs(specs)
+	registry.Register(ext)
+
+	root := &cobra.Command{
+		Long: "OLMv1 Origin Tests Extension",
+	}
+
+	root.AddCommand(cmd.DefaultExtensionCommands(registry)...)
+
+	if err := func() error {
+		return root.Execute()
+	}(); err != nil {
+		os.Exit(1)
+	}
+}
---
+++ openshift/extended-tests/go.mod
@@ -0,0 +1,42 @@
+module github/operator-framework-operator-controller/openshift/extended-tests
+
+go 1.23.6
+
+require (
+	github.com/openshift-eng/openshift-tests-extension v0.0.0-20250522124649-4ffcd156ec7c
+	github.com/spf13/cobra v1.8.1
+)
+
+require (
+	cel.dev/expr v0.18.0 // indirect
+	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-task/slim-sprig/v3 v3.0.0 // indirect
+	github.com/google/cel-go v0.17.8 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/pprof v0.0.0-20241029153458-d1b30febd7db // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/onsi/ginkgo/v2 v2.21.0 // indirect
+	github.com/onsi/gomega v1.35.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stoewer/go-strcase v1.2.0 // indirect
+	golang.org/x/exp v0.0.0-20230515195305-f3d0a9c9a5cc // indirect
+	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	golang.org/x/tools v0.26.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240826202546-f6391c0de4c7 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240924160255-9d4c2d233b61 // indirect
+	google.golang.org/protobuf v1.35.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+
+// cel-go v0.23.0 upgrade causes errors raised from the vendor source which lead to think in
+// incompatibilities scenarios. After upgrade to use the latest versions of k8s/api v0.33+
+// we should try to see if we could fix this one and remove this replace
+replace github.com/google/cel-go => github.com/google/cel-go v0.22.1
+
+replace k8s.io/api => k8s.io/api v0.32.3
+
+replace github.com/onsi/ginkgo/v2 => github.com/openshift/onsi-ginkgo/v2 v2.6.1-0.20241205171354-8006f302fd12
