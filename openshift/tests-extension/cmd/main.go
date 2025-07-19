63f04097a021a998ed50a4642ab4deea663f80e8
Camila Macedo <7708031+camilamacedo86@users.noreply.github.com>
2025-07-19 08:11:32 +0100

    UPSTREAM: <carry>: OTE add first test from openshift/origin olmv1.go

--- openshift/tests-extension/cmd/main.go
+++ openshift/tests-extension/cmd/main.go
@@ -21,6 +21,7 @@ import (
 
 	// The import below is necessary to ensure that the OLMv1 tests are registered with the extension.
 	_ "github/operator-framework-operator-controller/openshift/tests-extension/test"
+	"github/operator-framework-operator-controller/openshift/tests-extension/test/env"
 )
 
 func main() {
@@ -137,21 +138,10 @@ func main() {
 	// Add more removed test names below
 	)
 
-	// TODO: Init test framework for cluster-aware cases
-	// --------------------------------------------------
-	// The external binary doesn't currently init the test framework (e.g., kubeconfig, REST client).
-	// That's fine for now since our tests don't access the cluster.
-	// However, any test that does will fail when run with this binary.
-	//
-	// openshift-tests handles this via:
-	// - SuiteWithKubeTestInitializationPreSuite()
-	//   -> calls DecodeProvider() and InitializeTestFramework()
-	//
-	// We'll need to add similar logic when we start add the tests here.
-	//
-	// References:
-	// - https://github.com/openshift/origin/blob/main/pkg/cmd/openshift-tests/run/flags.go#L53
-	// - https://github.com/openshift/origin/blob/main/pkg/clioptions/clusterdiscovery/provider.go#L100
+	// Initialize the environment before running any tests.
+	specs.AddBeforeAll(func() {
+		env.Init()
+	})
 
 	ext.AddSpecs(specs)
 	registry.Register(ext)
