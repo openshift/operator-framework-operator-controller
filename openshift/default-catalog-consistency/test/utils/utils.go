3b71d8cb3db19ce2e26e758a8c865495a029cc31
Camila Macedo <7708031+camilamacedo86@users.noreply.github.com>
2025-10-13 18:47:53 +0100

    UPSTREAM: <carry>: [Default Catalog Tests] - Change logic to get ocp images from openshift/catalogd/manifests.yaml

--- openshift/default-catalog-consistency/test/utils/utils.go
+++ openshift/default-catalog-consistency/test/utils/utils.go
@@ -1,68 +1,115 @@
 package utils
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
+	"io"
 	"io/fs"
 	"os"
 	"path/filepath"
 	"regexp"
 	"strings"
 
-	"sigs.k8s.io/yaml"
+	yamlutil "k8s.io/apimachinery/pkg/util/yaml"
 
 	apiv1 "github.com/operator-framework/operator-controller/api/v1"
 )
 
 // ParseImageRefsFromCatalog reads the catalogs from the files used and returns a list of image references.
 func ParseImageRefsFromCatalog(catalogsPath string) ([]string, error) {
-	var images []string
-
 	re := regexp.MustCompile(`{{.*}}`)
 
-	// Check if the directory exists first
-	if _, err := os.Stat(catalogsPath); os.IsNotExist(err) {
-		return nil, fmt.Errorf("catalogs path %s does not exist", catalogsPath)
+	info, err := os.Stat(catalogsPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, fmt.Errorf("catalogs path %s does not exist", catalogsPath)
+		}
+		return nil, err
 	}
 
-	err := filepath.WalkDir(catalogsPath, func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
+	var (
+		images []string
+	)
 
-		if d.IsDir() || (!strings.HasSuffix(d.Name(), ".yaml") &&
-			!strings.HasSuffix(d.Name(), ".yml")) {
-			return nil
-		}
-
-		content, err := os.ReadFile(path)
-		if err != nil {
-			return nil
-		}
-		// Replace any helm templating
-		content = re.ReplaceAll(content, []byte{})
-
-		var catalog apiv1.ClusterCatalog
-		if err := yaml.Unmarshal(content, &catalog); err != nil {
-			return nil
-		}
-
-		if catalog.Kind != "ClusterCatalog" {
-			return nil
-		}
-
-		if catalog.Spec.Source.Type == apiv1.SourceTypeImage && catalog.Spec.Source.Image != nil {
-			images = append(images, catalog.Spec.Source.Image.Ref)
-		}
-
-		return nil
-	})
-
+	if info.IsDir() {
+		images, err = parseCatalogDir(catalogsPath, re)
+	} else {
+		images, err = parseCatalogFile(catalogsPath, re)
+	}
 	if err != nil {
 		return nil, err
 	}
 
 	if len(images) == 0 {
 		return nil, fmt.Errorf("no images found under catalogs path %s", catalogsPath)
+	}
+
+	return images, nil
+}
+
+func parseCatalogDir(root string, re *regexp.Regexp) ([]string, error) {
+	var images []string
+
+	err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if d.IsDir() {
+			return nil
+		}
+
+		if !strings.HasSuffix(d.Name(), ".yaml") && !strings.HasSuffix(d.Name(), ".yml") {
+			return nil
+		}
+
+		refs, err := parseCatalogFile(path, re)
+		if err != nil {
+			return err
+		}
+
+		images = append(images, refs...)
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return images, nil
+}
+
+func parseCatalogFile(path string, re *regexp.Regexp) ([]string, error) {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	return parseClusterCatalogs(content, re)
+}
+
+func parseClusterCatalogs(content []byte, re *regexp.Regexp) ([]string, error) {
+	// Replace any helm templating
+	content = re.ReplaceAll(content, []byte{})
+
+	decoder := yamlutil.NewYAMLOrJSONDecoder(bytes.NewReader(content), 4096)
+
+	var images []string
+	for {
+		var catalog apiv1.ClusterCatalog
+		if err := decoder.Decode(&catalog); err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return nil, err
+		}
+
+		if catalog.Kind != "ClusterCatalog" {
+			continue
+		}
+
+		if catalog.Spec.Source.Type == apiv1.SourceTypeImage && catalog.Spec.Source.Image != nil {
+			images = append(images, catalog.Spec.Source.Image.Ref)
+		}
 	}
 
 	return images, nil
