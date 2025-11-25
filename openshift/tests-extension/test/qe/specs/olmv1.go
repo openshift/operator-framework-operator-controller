f38d8a4500b6b3e60c8aa99c9a88644795b665cc
Xia Zhao <xzha@redhat.com>
2025-11-25 22:19:03 +0800

    UPSTREAM: <carry>: migrate olmv1 QE cases

--- openshift/tests-extension/test/qe/specs/olmv1.go
+++ openshift/tests-extension/test/qe/specs/olmv1.go
@@ -1,10 +1,15 @@
 package specs
 
 import (
+	"context"
+	"fmt"
 	"strings"
+	"time"
 
 	g "github.com/onsi/ginkgo/v2"
 	o "github.com/onsi/gomega"
+	"github.com/tidwall/gjson"
+	"k8s.io/apimachinery/pkg/util/wait"
 	e2e "k8s.io/kubernetes/test/e2e/framework"
 
 	exutil "github.com/openshift/operator-framework-operator-controller/openshift/tests-extension/test/qe/util"
@@ -38,6 +43,113 @@ var _ = g.Describe("[sig-olmv1][Jira:OLM] cluster-olm-operator", g.Label("NonHyp
 				e2e.Failf("the APIV1MetasHandler argument enabled in the general cluster: %v", args)
 			}
 		}
+	})
+
+	g.It("PolarionID:78393-[OTP][Skipped:Disconnected]support metrics", func() {
+		exutil.SkipOnProxyCluster(oc)
+
+		var metricsMsg string
+		g.By("get catalogd metrics")
+		promeEp, err := oc.WithoutNamespace().AsAdmin().Run("get").Args("service", "-n", "openshift-catalogd", "catalogd-service", "-o=jsonpath={.spec.clusterIP}").Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(promeEp).NotTo(o.BeEmpty())
+		if strings.Count(promeEp, ":") >= 2 {
+			g.Skip("Skip for IPv6.")
+		}
+		queryContent := "https://" + promeEp + ":7443/metrics"
+
+		g.By("Get token")
+		metricsToken, err := exutil.GetSAToken(oc)
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(metricsToken).NotTo(o.BeEmpty())
+
+		wrongToken, err := oc.AsAdmin().WithoutNamespace().Run("create").Args("token", "openshift-state-metrics", "-n", "openshift-monitoring").Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(wrongToken).NotTo(o.BeEmpty())
+
+		g.By("Get metrics")
+		podnameStr, _ := oc.AsAdmin().WithoutNamespace().Run("get").Args("pod", "-n", "openshift-monitoring", "-l", "prometheus==k8s", "-o=jsonpath='{..metadata.name}'").Output()
+		o.Expect(podnameStr).NotTo(o.BeEmpty())
+		prometheusPodname := strings.Split(strings.Trim(podnameStr, "'"), " ")[0]
+
+		errWait := wait.PollUntilContextTimeout(context.TODO(), 10*time.Second, 30*time.Second, false, func(ctx context.Context) (bool, error) {
+			metricsMsg, err := oc.AsAdmin().NotShowInfo().WithoutNamespace().Run("exec").Args("-n", "openshift-monitoring", prometheusPodname, "-i", "--", "curl", "-k", "-H", fmt.Sprintf("Authorization: Bearer %v", metricsToken), queryContent).Output()
+			e2e.Logf("err:%v", err)
+			if strings.Contains(metricsMsg, "catalogd_http_request_duration_seconds_bucket{code=\"200\"") {
+				e2e.Logf("found catalogd_http_request_duration_seconds_bucket{code=\"200\"")
+				return true, nil
+			}
+			return false, nil
+		})
+		if errWait != nil {
+			e2e.Logf("metricsMsg:%v", metricsMsg)
+			exutil.AssertWaitPollNoErr(errWait, "catalogd_http_request_duration_seconds_bucket{code=\"200\" not found.")
+		}
+
+		g.By("ClusterRole/openshift-state-metrics has no rule to get the catalogd metrics")
+		metricsMsg, _ = oc.AsAdmin().NotShowInfo().WithoutNamespace().Run("exec").Args("-n", "openshift-monitoring", prometheusPodname, "-i", "--", "curl", "-k", "-H", fmt.Sprintf("Authorization: Bearer %v", wrongToken), queryContent).Output()
+		o.Expect(metricsMsg).To(o.ContainSubstring("Authorization denied"))
+
+		g.By("get operator-controller metrics")
+		promeEp, err = oc.WithoutNamespace().AsAdmin().Run("get").Args("service", "-n", "openshift-operator-controller", "operator-controller-service", "-o=jsonpath={.spec.clusterIP}").Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(promeEp).NotTo(o.BeEmpty())
+		queryContent = "https://" + promeEp + ":8443/metrics"
+
+		errWait = wait.PollUntilContextTimeout(context.TODO(), 10*time.Second, 30*time.Second, false, func(ctx context.Context) (bool, error) {
+			metricsMsg, err := oc.AsAdmin().NotShowInfo().WithoutNamespace().Run("exec").Args("-n", "openshift-monitoring", prometheusPodname, "-i", "--", "curl", "-k", "-H", fmt.Sprintf("Authorization: Bearer %v", metricsToken), queryContent).Output()
+			e2e.Logf("err:%v", err)
+			if strings.Contains(metricsMsg, "controller_runtime_active_workers") {
+				e2e.Logf("found controller_runtime_active_workers")
+				return true, nil
+			}
+			return false, nil
+		})
+		if errWait != nil {
+			e2e.Logf("metricsMsg:%v", metricsMsg)
+			exutil.AssertWaitPollNoErr(errWait, "controller_runtime_active_workers not found.")
+		}
+
+		g.By("ClusterRole/openshift-state-metrics has no rule to get the operator-controller metrics")
+		metricsMsg, _ = oc.AsAdmin().NotShowInfo().WithoutNamespace().Run("exec").Args("-n", "openshift-monitoring", prometheusPodname, "-i", "--", "curl", "-k", "-H", fmt.Sprintf("Authorization: Bearer %v", wrongToken), queryContent).Output()
+		o.Expect(metricsMsg).To(o.ContainSubstring("Authorization denied"))
+
+	})
+
+	g.It("PolarionID:79770-[OTP][Level0]metrics are collected by default", func() {
+		podnameStr, _ := oc.AsAdmin().WithoutNamespace().Run("get").Args("pod", "-n", "openshift-monitoring", "-l", "prometheus==k8s", "-o=jsonpath='{..metadata.name}'").Output()
+		o.Expect(podnameStr).NotTo(o.BeEmpty())
+		k8sPodname := strings.Split(strings.Trim(podnameStr, "'"), " ")[0]
+
+		g.By("1) check status of Metrics targets is up")
+		targetsUrl := "http://localhost:9090/api/v1/targets"
+		targetsContent, _ := oc.AsAdmin().WithoutNamespace().Run("exec").Args("-n", "openshift-monitoring", k8sPodname, "--", "curl", "-s", targetsUrl).Output()
+		status := gjson.Get(targetsContent, `data.activeTargets.#(labels.namespace=="openshift-catalogd").health`).String()
+		if strings.Compare(status, "up") != 0 {
+			statusAll := gjson.Get(targetsContent, `data.activeTargets.#(labels.namespace=="openshift-catalogd")`).String()
+			e2e.Logf("catalogd target status: %s", statusAll)
+			o.Expect(status).To(o.Equal("up"))
+		}
+		status = gjson.Get(targetsContent, `data.activeTargets.#(labels.namespace=="openshift-operator-controller").health`).String()
+		if strings.Compare(status, "up") != 0 {
+			statusAll := gjson.Get(targetsContent, `data.activeTargets.#(labels.namespace=="openshift-operator-controller")`).String()
+			e2e.Logf("operator-controller target status: %s", statusAll)
+			o.Expect(status).To(o.Equal("up"))
+		}
+
+		g.By("2) check metrics are collected")
+		queryUrl := "http://localhost:9090/api/v1/query"
+		query1 := `query=catalogd_http_request_duration_seconds_count{code="200"}`
+		queryResult1, _ := oc.AsAdmin().WithoutNamespace().Run("exec").Args("-n", "openshift-monitoring", k8sPodname, "--", "curl", "-G", "--data-urlencode", query1, queryUrl).Output()
+		e2e.Logf("query result 1: %s", queryResult1)
+		o.Expect(queryResult1).To(o.ContainSubstring("value"))
+
+		query2 := `query=controller_runtime_reconcile_total{controller="controller-operator-cluster-extension-controller",result="success"}`
+		queryResult2, _ := oc.AsAdmin().WithoutNamespace().Run("exec").Args("-n", "openshift-monitoring", k8sPodname, "--", "curl", "-G", "--data-urlencode", query2, queryUrl).Output()
+		e2e.Logf("query result 2: %s", queryResult2)
+		o.Expect(queryResult2).To(o.ContainSubstring("value"))
+
+		g.By("3) test SUCCESS")
 	})
 
 })
