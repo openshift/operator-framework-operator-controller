2b4d1e471d6bd8cf3f8d3ecfcccfdd48e8802ad1
Per Goncalves da Silva <pegoncal@redhat.com>
2026-01-30 15:04:08 +0100

    UPSTREAM: <carry>: Add boxcutter specific preflight auth test
    Signed-off-by: Per Goncalves da Silva <pegoncal@redhat.com>

--- openshift/tests-extension/pkg/helpers/feature_capability.go
+++ openshift/tests-extension/pkg/helpers/feature_capability.go
@@ -2,6 +2,7 @@ package helpers
 
 import (
 	"context"
+	"fmt"
 
 	//nolint:staticcheck // ST1001: dot-imports for readability
 	. "github.com/onsi/ginkgo/v2"
@@ -23,18 +24,79 @@ func RequireOLMv1CapabilityOnOpenshift() {
 		extlogs.Warn("Skipping feature capability check: not OpenShift")
 		return
 	}
-
 	ctx := context.TODO()
-	clientset, err := configclient.NewForConfig(env.Get().RestCfg)
-	Expect(err).NotTo(HaveOccurred(), "failed to create config client")
-
+	clientset := configv1Client()
 	cv, err := clientset.ConfigV1().ClusterVersions().Get(ctx, "version", metav1.GetOptions{})
 	Expect(err).NotTo(HaveOccurred(), "failed to retrieve cluster version")
 
-	for _, cap := range cv.Status.Capabilities.EnabledCapabilities {
-		if cap == configv1.ClusterVersionCapabilityOperatorLifecycleManagerV1 {
+	for _, capability := range cv.Status.Capabilities.EnabledCapabilities {
+		if capability == configv1.ClusterVersionCapabilityOperatorLifecycleManagerV1 {
 			return
 		}
 	}
 	Skip("Test requires OperatorLifecycleManagerV1 capability")
+}
+
+// RequireFeatureGateEnabled requires featureGate to be in the list of enabled feature gates
+// for the OCP version in env.Get().OpenShiftVersion in the status of the
+// featuregates.config.openshift.io cluster resource
+func RequireFeatureGateEnabled(featureGate configv1.FeatureGateName) {
+	if !IsFeatureGateEnabled(featureGate) {
+		Skip(fmt.Sprintf("Test requires %q feature gate to be enabled", featureGate))
+	}
+}
+
+// RequireFeatureGateDisabled requires featureGate to be in the list of disabled feature gates
+// for the OCP version in env.Get().OpenShiftVersion in the status of the
+// featuregates.config.openshift.io cluster resource
+func RequireFeatureGateDisabled(featureGate configv1.FeatureGateName) {
+	if IsFeatureGateEnabled(featureGate) {
+		Skip(fmt.Sprintf("Test requires %q feature gate to be disabled", featureGate))
+	}
+}
+
+func IsFeatureGateEnabled(featureGate configv1.FeatureGateName) bool {
+	featureGates := getFeatureGateDetails(configv1Client())
+	for _, fg := range featureGates.Enabled {
+		if fg.Name == featureGate {
+			return true
+		}
+	}
+	return false
+}
+
+func getFeatureGateDetails(clientset *configclient.Clientset) configv1.FeatureGateDetails {
+	ctx := context.TODO()
+	featureGates, err := clientset.ConfigV1().FeatureGates().Get(ctx, "cluster", metav1.GetOptions{})
+	Expect(err).NotTo(HaveOccurred(), "failed to retrieve feature gates")
+
+	openshiftVersion := getOpenShiftVersion(clientset)
+	for _, fgs := range featureGates.Status.FeatureGates {
+		if fgs.Version == openshiftVersion {
+			return fgs
+		}
+	}
+	Fail(fmt.Sprintf("No feature gates found for version %s", openshiftVersion))
+	return configv1.FeatureGateDetails{}
+}
+
+func getOpenShiftVersion(clientset *configclient.Clientset) string {
+	// Fetch the single global 'version' resource
+	cv, err := clientset.ConfigV1().ClusterVersions().Get(context.TODO(), "version", metav1.GetOptions{})
+	Expect(err).NotTo(HaveOccurred(), "failed to retrieve cluster version")
+
+	// The status.history is a slice of updates.
+	// The first element (index 0) is the current or most recent version.
+	if len(cv.Status.History) > 0 {
+		return cv.Status.History[0].Version
+	}
+
+	Fail("No history found in cluster version status")
+	return ""
+}
+
+func configv1Client() *configclient.Clientset {
+	clientset, err := configclient.NewForConfig(env.Get().RestCfg)
+	Expect(err).NotTo(HaveOccurred(), "failed to create config client")
+	return clientset
 }
